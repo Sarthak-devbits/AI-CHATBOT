b2a5be4784f1b8038715f0c604d34e7dd01e4c53
Sarthak-devbits <snai@ciklum.com>
2025-08-02 13:39:28 +0530

    add tiktoken dependency and implement context length management in chat completion

--- src/basic_chat/index.ts
+++ src/basic_chat/index.ts
@@ -1,8 +1,12 @@
 import { OpenAI } from "openai";
+import { encoding_for_model } from "tiktoken";
 
 const openai = new OpenAI({
   apiKey: process.env.OPEN_API_KEY,
 });
+
+const MAX_TOKENS = 700;
+const encoder = encoding_for_model("gpt-3.5-turbo");
 
 let context: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
   {
@@ -20,11 +24,50 @@ async function createChatCompletion() {
     role: "assistant",
     content: response.choices[0].message.content,
   });
-  console.log(response.choices[0].message.content);
+  console.log(getContextLength());
+  if (
+    response &&
+    response?.usage &&
+    response?.usage?.total_tokens > MAX_TOKENS
+  ) {
+    console.log("yes");
+    deleteOlderMessages();
+  }
+}
+
+function deleteOlderMessages() {
+  let contextLength = getContextLength();
+  while (contextLength > MAX_TOKENS) {
+    for (let i = 0; i < context.length; i++) {
+      const message = context[i];
+      if (message.role != "system") {
+        context.slice(i, 1);
+        contextLength = getContextLength();
+        break;
+      }
+    }
+  }
+}
+
+function getContextLength() {
+  let length = 0;
+  context.forEach((message) => {
+    if (typeof message?.content == "string") {
+      length += encoder.encode(message.content).length;
+    } else if (Array.isArray(message?.content)) {
+      message?.content.forEach((item) => {
+        if (item.type == "text") {
+          length += encoder.encode(item.text).length;
+        }
+      });
+    }
+  });
+  return length;
 }
 
 process.stdin.addListener("data", async function (data) {
   const userInput = data.toString().trim();
+  console.log(context);
   context.push({
     role: "user",
     content: userInput,
